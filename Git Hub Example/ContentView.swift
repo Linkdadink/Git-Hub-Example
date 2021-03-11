8c8623e7fbe7848fde0b671c0000820ad8adf62a
Lincoln Blouin <s018351@students.lmsd.org>
2021-03-11 12:11:42 -0500

    inital commit

---
+++ Git Hub Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git Hub Example
+//
+//  Created by Lincoln Blouin (student LM) on 3/11/21.
+//  Copyright © 2021 Lincoln Blouin (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
